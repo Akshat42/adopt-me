0d9ec3896fb6e43ff40b1ce775cf5c1a8bb4ad80
Akshat42 <akshatdivya03@gmail.com>
2021-12-24 12:42:17 +0530

    initial checkin

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import SearchParams from "./SearchParams";
+
+// const App = () => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h1", {}, "Adopt Me!"),
+//     React.createElement(Pet, {
+//       name: "Rose",
+//       animal: "Dog",
+//       breed: "Labrador",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Anmoli",
+//       animal: "Dog",
+//       breed: "Chihuahua",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Mithu",
+//       animal: "Bird",
+//       breed: "Indian Parrot",
+//     }),
+//   ]);
+// };
+
+const App = () => {
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ src/SearchParams.js
@@ -0,0 +1,94 @@
+import { useEffect, useState } from "react";
+import Pet from "./Pet";
+
+const ANIMALS = ["dog", "bird", "cat", "reptiles", "fish"];
+
+const SearchParams = () => {
+    const [location, setLocation] = useState("");
+    const [animal, setAnimal] = useState("dog");
+    const [breedList, setBreedList] = useState([]);
+    const [pets, setPets] = useState([]);
+    const [breed, setBreed] = useState("");
+
+    useEffect(() => {
+        requestPets();
+    },[]);
+
+    async function requestPets() {
+        const res = await fetch(
+            `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+        );
+        const json = await res.json();
+        setPets(json.pets);
+    }
+
+    useEffect(() => {
+        requestBreeds();
+    },[animal]);
+
+    async function requestBreeds() {
+        const res = await fetch(
+            `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+        )
+        const json = await res.json();
+        if (json.breeds)
+            setBreedList(json.breeds);
+        else
+            setBreedList([]);
+    }
+
+    return (
+        <div className = "search-params">
+        <form onSubmit = {e => {
+            e.preventDefault();
+            requestPets();
+        }}>
+            <label htmlFor = "location">
+                <input id = "location" onChange = {e => setLocation(e.target.value)} value = {location} placeholder = "Location" />
+            </label>
+            <label htmlFor= "animal">
+            Animal
+            <select 
+            id = "animal"
+            value = {animal}
+            onChange = {e => setAnimal(e.target.value)}
+            onBlur = {e => setAnimal(e.target.value)}>
+            <option value = ""></option>
+            {
+                ANIMALS.map(animal => (
+                    <option 
+                     value = {animal}
+                     key = {animal}>{animal}</option>
+                ))  
+            }
+            </select>
+            </label>
+            <label htmlFor= "breed">
+            Breed
+            <select 
+            id = "breed"
+            value = {breed}
+            onChange = {e => setBreed(e.target.value)}
+            onBlur = {e => setBreed(e.target.value)}>
+            <option value = ""></option>
+            {
+                breedList.map(breed => (
+                    <option 
+                     value = {breed}
+                     key = {breed}>{breed}</option>
+                ))  
+            }
+            </select>
+            </label>
+            <button>Submit</button>
+        </form>
+        {
+            pets.map(pet => (
+                <Pet key = {pet.id} name = {pet.name} animal = {pet.animal} breed = {pet.breed}></Pet>
+            ))
+        }
+        </div>
+    )
+}
+
+export default SearchParams;
