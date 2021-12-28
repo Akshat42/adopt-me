51413c6028fd5f3be339f657d8088d1758d11b7e
Akshat42 <akshatdivya03@gmail.com>
2021-12-28 13:24:36 +0530

    add image carousel

--- src/Details.js
+++ src/Details.js
@@ -1,42 +1,46 @@
 import { Component } from "react";
 import { withRouter } from "react-router-dom";
-
+import Carousel from "./Carousel";
 
 class Details extends Component {
+  constructor() {
+    super();
+    this.state = { loading: true };
+    console.log(this.state);
+  }
 
-    constructor() {
-        super();
-        this.state = {loading: true};
-        console.log(this.state);
-    }
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+    );
+    const json = await res.json();
+    this.setState(
+      Object.assign(
+        {
+          loading: false,
+        },
+        json.pets[0]
+      )
+    );
+  }
 
-    async componentDidMount() {
-        const res = await fetch(
-            `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
-        )
-        const json = await res.json();
-        this.setState(
-            Object.assign({
-                loading: false
-            },
-            json.pets[0]
-            )
-        );
-    }
-
-    render() {
-        const {animal, name, breed, city, state, description } = this.state;
-        return (
-            <div className="details">
-                <div>
-                    <h1>{name}</h1>
-                    <h2>{animal} - {breed} - {city}, {state}</h2>
-                    <button>Adopt {name}</button>
-                </div>
-                <p>{description}</p>
-            </div>
-        )
-    }
+  render() {
+    const { animal, name, breed, city, state, description, images } =
+      this.state;
+    return (
+      <div className="details">
+        <Carousel images={images} />
+        <div>
+          <h1>{name}</h1>
+          <h2>
+            {animal} - {breed} - {city}, {state}
+          </h2>
+          <button>Adopt {name}</button>
+        </div>
+        <p>{description}</p>
+      </div>
+    );
+  }
 }
- 
+
 export default withRouter(Details);
