ffb22a6dd95879452cb86db9e9edf8ac84597fd2
Akshat42 <akshatdivya03@gmail.com>
2021-12-29 13:29:55 +0530

    add modal

--- src/Details.js
+++ src/Details.js
@@ -2,12 +2,13 @@ import { Component } from "react";
 import { withRouter } from "react-router-dom";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
+import Modal from "./Modal";
 import ThemeContext from "./ThemeContext";
 
 class Details extends Component {
   constructor() {
     super();
-    this.state = { loading: true };
+    this.state = { loading: true, showModal: false };
   }
 
   async componentDidMount() {
@@ -25,6 +26,9 @@ class Details extends Component {
     );
   }
 
+  toggleModal = () => this.setState({showModal: !this.state.showModal});
+  adopt = () => (window.location= "https://bit.ly/pet-adopt"); 
+
   render() {
     const { animal, name, breed, city, state, description, images } =
       this.state;
@@ -38,11 +42,26 @@ class Details extends Component {
           </h2>
           <ThemeContext.Consumer>
             {([theme]) => (
-              <button style={{ backgroundColor: theme }}>Adopt {name}</button>
+              <button
+              onClick={this.toggleModal}
+               style={{ backgroundColor: theme }}>Adopt {name}</button>
             )}
           </ThemeContext.Consumer>
         </div>
         <p>{description}</p>
+        {
+          this.state.showModal ? (
+            <Modal>
+              <div>
+                <h1>Are you sure you want to adopt {name}</h1>
+                <div className="buttons">
+                  <button onClick={this.adopt}>Yes</button>
+                  <button onClick={this.toggleModal}>No</button>
+                </div>
+              </div>
+            </Modal>
+          ) : null
+        }
       </div>
     );
   }
